57fd4225287f11705a53145ca201575fb73cc7c5
wafferdp <jacob.brannstrom@gmail.com>
2015-04-01 15:05:21 +0200

    mongoose client instead of dumb mongo

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express        = require('express');
 var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-var MongoClient    = require('mongodb').MongoClient;
-var ObjectID	   = require('mongodb').ObjectID;
-var Mongoose       = require('mongoose');
+var MongoDb        = require('mongodb');
+var MongoClient    = MongoDb.MongoClient;
+var ObjectID	   = MongoDb.ObjectID;
 
+var mongoose       = require('mongoose');
 var app            = express();
 
 app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
@@ -15,54 +16,71 @@ app.use(methodOverride()); 					// simulate DELETE and PUT
 
 var db = {};
 
-var Book = new Schema({  
-    title: { type: String, required: true },  
-    author: { type: String, required: true },  
-    isbn: { type: String },
-    cover: { meta: string, data: string},
-    date: { type: Date, default: Date.now }
+mongoose.connect("mongodb://localhost/library");
+
+var BookSchema = new mongoose.Schema({  
+    title: String,  
+    author: String,  
+    isbn: String,
+    date: Date 
+},{
+    collection: 'books'
 });
 
+var BookModel = new mongoose.model('book',BookSchema);
 
-MongoClient.connect("mongodb://localhost:27017/library", function(err, successDb) {
-	db = successDb;
-});
+
+
+//MongoClient.connect("mongodb://localhost:27017/library", function(err, successDb) {
+//    db = successDb;
+//});
 
 app.get('/api/library', function(req, res) {
-	if(db != null)
-	{
-		db.collection('books', function(err, collection) {
-			var allBooks = collection.find().toArray(function(err, items) {
-				res.send(JSON.stringify(items));
-			});
-		});
-	}	
+    if(db != null)
+    {
+	db.collection('books', function(err, collection) {
+	    var allBooks = collection.find().toArray(function(err, items) {
+		res.send(JSON.stringify(items));
+	    });
+	});
+    }	
 });
 
 app.post('/api/library', function(req,res) {
 
-	console.log(req.body);
-	if(db != null)
+    console.log(req.body);
+    var book = new BookModel(req.body);
+    book.save(function(err){
+	if(err)
 	{
-	    db.collection('books', function(err, collection) {
-			collection.insert(req.body);
-			console.log(req.body._id);
-			res.send(req.body._id);
-		});
+	    console.log("Error creating book: " + err);
 	}
+	else
+	{
+	    res.send(book._id);
+	}
+    });
+    if(db != null)
+    {
+	db.collection('books', function(err, collection) {
+	    collection.insert(req.body);
+	    console.log(req.body._id);
+	    res.send(req.body._id);
+	});
+    }
 });
 
 app.delete('/api/library/:id', function(req,res){
 
-	var query = {"_id" : new ObjectID(req.params.id)};
-	
-	if(db != null)
-	{
-		db.collection('books', function(err, collection) {
-			collection.remove(query, {justOne : false}, function(err, itemsRemoved) {
-			});
-		});
-	}
+    var query = {"_id" : new ObjectID(req.params.id)};
+    
+    if(db != null)
+    {
+	db.collection('books', function(err, collection) {
+	    collection.remove(query, {justOne : false}, function(err, itemsRemoved) {
+	    });
+	});
+    }
 });
 
 app.listen(8000);	
